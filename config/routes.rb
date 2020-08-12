b8a54f96d3380950f8f2d579d0ba65224b4fb9ec
Minh Vi Nguyen <minhvi@hotmail.de>
2020-08-12 16:18:43 +0200

    navbar and style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
+  end
+  root to: 'restaurants#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
